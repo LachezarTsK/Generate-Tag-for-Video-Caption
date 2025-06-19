1ff6ec86c978f251f228eaf32f1972147aab326c
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-06-19 18:47:36 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,32 @@
+
+/**
+ * @param {string} caption
+ * @return {string}
+ */
+var generateTag = function (caption) {
+    const EMPTY_SPACE = ' ';
+    const TAG_HEAD = '#';
+    const TAG_MAX_LENGTH = 100;
+
+    caption = caption.trim();
+    if (caption.length === 0) {
+        return TAG_HEAD;
+    }
+
+    const tagVideoCaption = new Array();
+    tagVideoCaption.push(TAG_HEAD);
+    tagVideoCaption.push((caption.charAt(0)).toLowerCase());
+
+    for (let i = 1; i < caption.length && tagVideoCaption.length < TAG_MAX_LENGTH; ++i) {
+        if (caption.charAt(i) === EMPTY_SPACE) {
+            continue;
+        }
+        if (caption.charAt(i - 1) === EMPTY_SPACE) {
+            tagVideoCaption.push((caption.charAt(i)).toUpperCase());
+            continue;
+        }
+        tagVideoCaption.push((caption.charAt(i)).toLowerCase());
+    }
+
+    return tagVideoCaption.join('');
+};
